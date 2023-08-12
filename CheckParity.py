a79e077a7c002419d2a071dd6dc7ce9cc32f675d
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-12 04:21:37 +0200

    Adding Number Parity Checker

---
+++ CheckParity.py
@@ -0,0 +1,11 @@
+def CheckEven(Number):
+    if (Number % 2) == 0:
+        return True
+    else:
+        return False
+
+def CheckOdd(Number):
+    if (Number % 2) != 0:
+        return True
+    else:
+        return False
